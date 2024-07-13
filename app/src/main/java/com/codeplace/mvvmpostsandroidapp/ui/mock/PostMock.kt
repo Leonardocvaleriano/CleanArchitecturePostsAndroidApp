fbd1b33d2638337547741e5efc46c867f517bcce
Leonardocvaleriano <leonardoconsanivaleriano@gmail.com>
2024-07-13 20:54:27 +0100

    initial commit

---
+++ app/src/main/java/com/codeplace/mvvmpostsandroidapp/ui/mock/PostMock.kt
@@ -0,0 +1,6 @@
+package com.codeplace.mvvmpostsandroidapp.ui.mock
+
+data class PostMock(
+    val title:String? = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    val body:String? = "Nullam vehicula ipsum a arcu cursus vitae. Malesuada bibendum arcu vitae elementum curabitur vitae nunc sed"
+)
